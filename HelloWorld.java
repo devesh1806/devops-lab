b40d9f43acd46ed6d42296b3c481483dfc7306f1
ankitk12029 <41890251+ankitk12029@users.noreply.github.com>
2021-05-20 10:00:03 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+// Java code to illustrate
+// System.out.println();
+  
+import java.io.*;
+  
+class GFG {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+    }
+}
